1b96e73dc408e47f261c83fe67192196e468f2d1
CarlosAndDevs <caguilarcalderon19@gmail.com>
2020-12-26 19:19:33 -0600

    agregando pruebas de test unitarios a inputs del formulario

---
+++ littleBoy/tests/ValidateTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Validate;
+
+class ValidateTest extends TestCase
+{
+    public function test_email()
+    {
+        $email = Validate::email('i@rimorsoft.com');
+        $this->assertTrue($email);
+
+        $email = Validate::email('i@@rimorsoft.com');
+        $this->assertFalse($email);
+    }
+
+    public function test_url()
+    {
+        $url = Validate::url('https://platzi.com');
+        $this->assertTrue($url);
+
+        $url = Validate::url('platzi.com');
+        $this->assertFalse($url);
+    }
+
+    public function test_password()
+    {
+        $password = Validate::password('Abc1234VS');
+        $this->assertTrue($password);
+
+        $password = Validate::password('Abc1w');
+        $this->assertFalse($password);
+    }
+}
